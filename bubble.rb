fdabfec376b21083c3333b7f93480d33723bae29
srappaport <sarah.rappaport@greendot.org>
2018-05-20 15:20:02 +0000

    automatically backed up by learn

---
+++ bubble.rb
@@ -0,0 +1,28 @@
+sequence = [4, 3, 5, 0, 1]
+swaps = 0
+
+# Your Code Here
+# start at beginning
+#if first num and second num don't swap, incremement and try next
+#if swap, return to beginning
+
+i = 0
+
+while i < 5
+if sequence[i] < sequence[i+1]
+  i +=1
+  puts sequence
+elsif sequence[i] > sequence[i+1]
+  a=sequence[i]
+  b=sequenc[i+1]
+  sequence[i] = b
+  sequence[i+1] =a
+  i = 0
+  puts sequence
+end
+  rescue ExceptionName
+    
+  end
+
+puts "Final result: #{result}"
+puts "Swaps: #{swaps}"
